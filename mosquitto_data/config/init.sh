056f2f473928d3e2562f279bdff057799d0f4acc
arizmuajianisan <arizmuajianisan@gmail.com>
2024-08-28 14:59:38 +0700

    adding set permission for init.sh

--- mosquitto_data/config/init.sh
+++ mosquitto_data/config/init.sh
@@ -5,6 +5,8 @@ if [ ! -f /mosquitto/config/pwfile ]; then
     echo "Creating new Mosquitto user..."
     # Create a new password file with the user and password from environment variables
     mosquitto_passwd -b -c /mosquitto/config/pwfile $MQTT_USER $MQTT_PASSWORD
+    # Set proper permissions
+    chmod 600 /mosquitto/config/pwfile
 else
     echo "Mosquitto password file already exists."
 fi
